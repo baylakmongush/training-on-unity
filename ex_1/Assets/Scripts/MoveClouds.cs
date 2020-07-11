47b0cbfa1f5d67c359aed92ddf437541f3488548
Ning Petrelli <npetrell@ga-s1.msk.21-school.ru>
2020-07-11 21:19:21 +0300

    add flowers, the main character(moving and animation), spawn clouds

---
+++ ex_1/Assets/Scripts/MoveClouds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveClouds : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    float       thrust = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x += Time.deltaTime * thrust;
+        transform.position = position;
+        if (gameObject.transform.position.x > Camera.main.ScreenToWorldPoint(new Vector2(Camera.main.pixelWidth, 0)).x * 2)
+            Destroy(gameObject);
+    }
+}
---
+++ ex_1/Assets/Scripts/SpawnClouds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnClouds : MonoBehaviour
+{
+    public GameObject  prefabClouds;
+    float       count = 10;
+    float       x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = prefabClouds.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 leftBorder = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+       // float leftBorder = Camera.main.ViewportToWorldPoint(Vector3(0, 0, prefabClouds.transform.position.z)).x;
+        if (count >= 10)
+        {
+            Instantiate(prefabClouds, new Vector3((leftBorder.x - prefabClouds.transform.position.x) * 2, Random.Range(-1f , 1f), prefabClouds.transform.position.z), Quaternion.identity);
+            count = 0;
+        }
+        count += Time.deltaTime;
+    }
+}
