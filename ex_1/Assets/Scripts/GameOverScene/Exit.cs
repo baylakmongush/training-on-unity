84825cd82db5bca651158949ee1ade33b3c23839
Ning Petrelli <npetrell@mi-q1.msk.21-school.ru>
2020-07-13 17:55:32 +0300

    Change button quit

--- ex_1/Assets/Scripts/GameOverScene/Exit.cs
+++ ex_1/Assets/Scripts/GameOverScene/Exit.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class Exit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     public void HandleQuitButtonOnClickEvent()
     {
         Application.Quit();
